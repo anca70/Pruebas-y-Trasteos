6890688836e561b9cd56d7487dbd361aeb3d3fc3
anca70 <jac70@gmx.es>
2020-03-05 13:44:53 +0100

    Cambios

--- Pruebas/Hola.py
+++ Pruebas/Hola.py
@@ -1,8 +1,14 @@
+<<<<<<< HEAD
 <<<<<<< Updated upstream
 =======
+=======
+>>>>>>> anca70/master
 print('Hola Mundo')
 
 print('Otro cambio')
 
 print('Cambio desde GitHub')
+<<<<<<< HEAD
 >>>>>>> Stashed changes
+=======
+>>>>>>> anca70/master
